1049a96e31293b8a3b278eaafe79ad37bf859875
Brocier <jschoe04@gmail.com>
2018-05-04 13:25:02 -0400

    calling it a day

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,11 +1,15 @@
 import {createStore, applyMiddleware} from 'redux'
+import {createLogger} from 'redux-logger'
+import createSagaMiddleware, {END} from 'redux-saga'
+import reducer from './reducers'
+
 import {routerMiddleware} from 'react-router-redux'
 import {composeWithDevTools} from 'redux-devtools-extension/logOnlyInProduction'
-import thunk from 'redux-thunk'
-import {createLogger} from 'redux-logger'
-import reducer from './reducers' // Or wherever you keep your reducers
+// import thunk from 'redux-thunk'
 
 export default function configureStore(history) {
+  // Apply saga middleware
+  const sagaMiddleware = createSagaMiddleware()
 
   // Apply navigation middleware
   const routerHistory = routerMiddleware(history)
@@ -13,7 +17,14 @@ export default function configureStore(history) {
   // Middleware for logging redux action to console
   const logger = createLogger({collapsed: true})
 
-  const store = createStore(reducer, composeWithDevTools(applyMiddleware(...[thunk, routerHistory, logger])))
+  const store = createStore(reducer, composeWithDevTools(applyMiddleware(...[sagaMiddleware, routerHistory, logger])))
+
+  store.runSaga = sagaMiddleware.run
+  store.close = () => store.dispatch(END)
 
   return store
 }
+
+// Borrowed heavily from
+// https://github.com/redux-saga/redux-saga/blob/master/examples/real-world/stor
+// e/configureStore.dev.js
